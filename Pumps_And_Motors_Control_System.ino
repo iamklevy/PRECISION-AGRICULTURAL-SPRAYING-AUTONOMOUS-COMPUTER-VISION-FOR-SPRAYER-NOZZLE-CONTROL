9d6d4a325bbfe6f93711b888d069e61285de8f07
klevy <94145850+iamklevy@users.noreply.github.com>
2024-06-18 19:29:31 +0300

    Add files via upload

---
+++ Pumps_And_Motors_Control_System.ino
@@ -0,0 +1,73 @@
+#define BLYNK_TEMPLATE_ID "TMPL2yTVDjK7B"
+#define BLYNK_TEMPLATE_NAME "Precision Agricultural spraying"
+#define BLYNK_AUTH_TOKEN "ErmQvrBJiKgkwGizEu7fAGZv4ZkjmAne"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "SSID";
+char pass[] = "password";
+
+int IN1 = 4;
+int IN2 = 5;
+int IN3 = 12;
+int IN4 = 13;
+int ENA = 18;
+int ENB = 19;
+
+
+BLYNK_WRITE(V1) {  //move forward
+  int state = param.asInt();
+  Serial.print("Move Forward: ");
+  digitalWrite(IN2, state);
+  digitalWrite(IN4, state);
+}
+
+BLYNK_WRITE(V2) {  //move backward
+  int state = param.asInt();
+  Serial.print("Move Backward: ");
+  digitalWrite(IN1, state);
+  digitalWrite(IN3, state);
+}
+
+BLYNK_WRITE(V3) {  //turn left
+  int state = param.asInt();
+  Serial.print("Turn Left: ");
+  digitalWrite(IN2, state);
+  digitalWrite(IN3, state);
+}
+
+BLYNK_WRITE(V4) {  //turn right
+  int state = param.asInt();
+  Serial.print("Turn Right: ");
+  digitalWrite(IN1, state);
+  digitalWrite(IN4, state);
+}
+
+BLYNK_WRITE(V5) {  //speed accelation
+  int speed = param.asInt();
+  analogWrite(ENA, speed);
+  analogWrite(ENB, speed);
+}
+
+void setup() {
+  // Debug console
+  Serial.begin(115200);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 8080);
+}
+
+void loop() {
+  Blynk.run();
+}
